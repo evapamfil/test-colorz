368690016914e246b155e3ab4ae102b73100875b
Eva Pamfil <evapamfil@macbook-pro-de-eva.home>
2021-04-14 01:07:10 +0200

    add files

---
+++ README.md
@@ -0,0 +1,14 @@
+## Test Colorz
+
+## Requirements
+- **NPM version** - 6.14.4
+- **NODE version** - 12.16.1
+
+## Installation and usage
+
+`git clone`
+`npm install`
+`gulp build`
+
+
+
---
+++ src/components/main.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('.burger').classList.toggle('open')
+    document.querySelector('.nav').classList.toggle('open')
+}, false)
+  
